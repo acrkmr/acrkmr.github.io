0bc10d3d7d4445ae111c026091a87fbbf3562e44
acrkmr <134840990+acrkmr@users.noreply.github.com>
2023-05-31 01:35:05 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,26 +130,17 @@
    * Hero type effect
    */
   const typed = select('.typed')
-if (typed) {
-  let typed_strings = typed.getAttribute('data-typed-items')
-  typed_strings = typed_strings.split(',')
-
-  new Typed('.typed', {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000,
-    // Add the following options to prevent text from moving on mobile devices
-    onReset: function() {
-      this.el.style.display = 'inline-block';
-    },
-    onStart: function() {
-      this.el.style.display = 'inline-block';
-    },
-  });
-}
-
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
 
   /**
    * Skills animation
